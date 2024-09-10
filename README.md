a7a9ddb4a963b82bc3a911303198d9dac5ae437c
Jayant Malik <dev.jayantmalik@gmail.com>
2024-09-10 21:30:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Open SMS
+
+Simple SMS application Free and Open Source with minimal features and clean user interface.
+
---
+++ app/src/main/java/com/developbharat/opensms/ui/MainApplication.kt
@@ -0,0 +1,5 @@
+package com.developbharat.opensms.ui
+
+import android.app.Application
+
+class MainApplication : Application()
